3b1ae0b5813136f7267b8a1e006ee201867dcd5e
Ahsan <ahsanali.bscssef20@iba-suk.edu.pk>
2023-09-26 14:20:04 +0500

    added task3

---
+++ assets/task3/task.js
@@ -0,0 +1,9 @@
+// 3. Here is a sample html file with a submit button. Now modify the style of the paragraph text (such as fontSize, fontFamily, color, etc. )
+// through javascript code.
+
+const js_style = ()=>{
+    const para = document.querySelector("p");
+    para.style.fontSize = "50px";
+    para.style.fontFamily = "Impact";
+    para.style.color = "Red";
+}
